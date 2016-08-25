c0a0417161d325e176604c9b47c64d09e0326dcf
heipacker <heipackermail@gmail.com>
2016-08-26 00:27:02 +0800

    new: 编译jar包

--- src/main/java/com/_37coins/bcJsonRpc/events/AlertListener.java
+++ src/main/java/com/_37coins/bcJsonRpc/events/AlertListener.java
@@ -1,44 +1,44 @@
 package com._37coins.bcJsonRpc.events;
 
+import com._37coins.bcJsonRpc.BitcoindClientFactory;
+
 import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
-import com._37coins.bcJsonRpc.BitcoindClientFactory;
-
-
 public class AlertListener extends Observable implements Observer {
 
-	final private Observable alertListener;
-	public Thread listener = null;
+    private final Observable alertListener;
 
-	public AlertListener() throws IOException {
-		if (BitcoindClientFactory.alertSocket!=null){
-			alertListener = new BitcoinDListener(BitcoindClientFactory.alertSocket);
-		}else{
-			alertListener = new BitcoinDListener(4003);
-		}
-	}
+    public Thread listener = null;
 
-	@Override
-	public synchronized void addObserver(Observer o) {
-		if (null == listener) {
-			alertListener.addObserver(this);
-			listener = new Thread((Runnable) alertListener, "alertListener");
-			listener.start();
-		}
-		super.addObserver(o);
-	}
+    public AlertListener() throws IOException {
+        if (BitcoindClientFactory.alertSocket != null) {
+            alertListener = new BitcoinDListener(BitcoindClientFactory.alertSocket);
+        } else {
+            alertListener = new BitcoinDListener(4003);
+        }
+    }
 
-	@Override
-	public void update(Observable o, Object arg) {
-		final String value = ((String) arg).trim();
-		setChanged();
-		notifyObservers(value);
-	}
-	
-	public void stop(){
-		listener.interrupt();
-	}
+    @Override
+    public synchronized void addObserver(Observer o) {
+        if (null == listener) {
+            alertListener.addObserver(this);
+            listener = new Thread((Runnable) alertListener, "alertListener");
+            listener.start();
+        }
+        super.addObserver(o);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        final String value = ((String) arg).trim();
+        setChanged();
+        notifyObservers(value);
+    }
+
+    public void stop() {
+        listener.interrupt();
+    }
 
 }
